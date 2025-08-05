38646f851c3a7652198f359151b3839833b4766c
Yuvraj Kumar <yuvrajkumar.dev@gmail.com>
2025-08-05 22:19:27 +0530

    add memory.py

---
+++ memory.py
@@ -0,0 +1,33 @@
+from langchain_community.document_loaders import TextLoader
+
+def create_vector_store():
+    #Data Ingestion
+
+    data = TextLoader("output.txt", encoding="utf-8")
+    docs = data.load()
+
+    #Chunks
+
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500, chunk_overlap=100)
+    text_chunks = text_splitter.split_documents(docs)
+
+    #Embeddings and Vector Store
+
+    from langchain_community.embeddings import HuggingFaceEmbeddings
+
+    # from langchain_community.embeddings import OllamaEmbeddings
+
+    from langchain_community.vectorstores import FAISS
+
+    # Step 1: Instantiate embedding model
+    embedding_model = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en")
+
+    # Step 2: Create vectorstore from documents
+    db = FAISS.from_documents(text_chunks, embedding_model)
+
+    # Step 3: Save vectorstore locally
+    db.save_local("faiss_index")
+
+create_vector_store()
